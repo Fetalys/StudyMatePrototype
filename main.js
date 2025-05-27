462d84a4c490cb18125f776e165a34b106adeef3
Aldrich <118922910+Fetalys@users.noreply.github.com>
2025-05-27 22:28:15 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-    function openWindow(app) {
+function openWindow(app) {
       const win = document.getElementById(`${app}-window`);
       win.classList.remove('hidden');
       makeDraggable(win);
@@ -12,12 +12,23 @@
       const header = el.querySelector(".window-header");
       let offsetX = 0, offsetY = 0, isDragging = false;
 
+      // pc functions
       header.onmousedown = function (e) {
         isDragging = true;
         offsetX = e.clientX - el.offsetLeft;
         offsetY = e.clientY - el.offsetTop;
         document.onmousemove = drag;
         document.onmouseup = stopDrag;
+      };
+
+      // phone|tablet functions
+      header.ontouchstart = function (e) {
+        isDragging = true;
+        const touch = e.touches[0];
+        offsetX = touch.clientX - el.offsetLeft;
+        offsetY = touch.clientY - el.offsetTop;
+        document.ontouchmove = touchDrag;
+        document.ontouchend = stopTouchDrag;
       };
 
       function drag(e) {
@@ -30,5 +41,18 @@
         isDragging = false;
         document.onmousemove = null;
         document.onmouseup = null;
+      }
+
+      function touchDrag(e) {
+        if (!isDragging) return;
+        const touch = e.touches[0];
+        el.style.left = touch.clientX - offsetX + "px";
+        el.style.top = touch.clientY - offsetY + "px";
+      }
+
+      function stopTouchDrag() {
+        isDragging = false;
+        document.ontouchmove = null;
+        document.ontouchend = null;
       }
     }
